df5852b88834387f89cd1beae252e567c8d846d8
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-03 21:43:13 +0000

    Removing mysql specific setup stuff. If somebody wants to keep their data, they will need to define their own handling.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/polonium/trunk@1540 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/polonium/test_case.rb
+++ lib/polonium/test_case.rb
@@ -14,25 +14,7 @@ module Polonium
 
     include SeleniumDsl
     def setup
-      #   set "setup_once" to true
-      #   to prevent fixtures from being re-loaded and data deleted from the DB.
-      #   this is handy if you want to generate a DB full of sample data
-      #   from the tests.  Make sure none of your selenium tests manually
-      #   reset data!
-      #TODO: make this configurable
-      setup_once = false
-
       raise "Cannot use transactional fixtures if ActiveRecord concurrency is turned on (which is required for Selenium tests to work)." if self.class.use_transactional_fixtures
-      unless setup_once
-        ActiveRecord::Base.connection.update('SET FOREIGN_KEY_CHECKS = 0')
-        super
-        ActiveRecord::Base.connection.update('SET FOREIGN_KEY_CHECKS = 1')
-      else
-        unless InstanceMethods.const_defined?("ALREADY_SETUP_ONCE")
-          super
-          InstanceMethods.const_set("ALREADY_SETUP_ONCE", true)
-        end
-      end
       @selenium_driver = configuration.driver
     end
 
