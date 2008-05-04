99d52b7a39de53b5bb73647c89d34a127148b1e7
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-04 07:57:54 +0000

    Added Spec::Runner::Options#stop_selenium. Fixed usage of after(:suite) callback.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/polonium/trunk@1543 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/polonium/adapters/rspec.rb
+++ lib/polonium/adapters/rspec.rb
@@ -1,16 +1,20 @@
 class Spec::Runner::Options
   attr_accessor :selenium_configuration, :selenium_app_runner
 
+  def stop_selenium(success)
+    selenium_app_runner.stop
+    selenium_configuration.stop_driver_if_necessary(success)
+    success
+  end
+
   if instance_methods.include?('after_suite_parts')
-    ExampleGroupMethods.after(:suite) do |success|
-      selenium_app_runner.stop
-      selenium_configuration.stop_driver_if_necessary(success)
+    Spec::Example::ExampleGroup.after(:suite) do |success|
+      rspec_options.stop_selenium success
     end
   else
     def run_examples_with_selenium_runner(*args)
       success = run_examples_without_selenium_runner(*args)
-      selenium_app_runner.stop
-      selenium_configuration.stop_driver_if_necessary(success)
+      stop_selenium success
       success
     end
     alias_method_chain :run_examples, :selenium_runner
