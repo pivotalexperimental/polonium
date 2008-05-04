529e4660c651e9c23a342b34e0c5e5d8a3b9ac5b
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-04 05:58:06 +0000

    Using rspec's after(:suite) callbacks if available.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/polonium/trunk@1542 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/polonium/adapters/rspec.rb
+++ lib/polonium/adapters/rspec.rb
@@ -1,13 +1,20 @@
 class Spec::Runner::Options
   attr_accessor :selenium_configuration, :selenium_app_runner
 
-  def run_examples_with_selenium_runner(*args)
-    success = run_examples_without_selenium_runner(*args)
-    selenium_app_runner.stop
-    selenium_configuration.stop_driver_if_necessary(success)
-    success
+  if instance_methods.include?('after_suite_parts')
+    ExampleGroupMethods.after(:suite) do |success|
+      selenium_app_runner.stop
+      selenium_configuration.stop_driver_if_necessary(success)
+    end
+  else
+    def run_examples_with_selenium_runner(*args)
+      success = run_examples_without_selenium_runner(*args)
+      selenium_app_runner.stop
+      selenium_configuration.stop_driver_if_necessary(success)
+      success
+    end
+    alias_method_chain :run_examples, :selenium_runner
   end
-  alias_method_chain :run_examples, :selenium_runner
 end
 
 rspec_options.selenium_configuration = Polonium::Configuration.instance
