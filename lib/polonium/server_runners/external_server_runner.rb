eab502efd04f7a0c46b99eda8ce17434773f833e
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-07-18 23:26:19 +0000

    * ExternalServerRunner start_server and stop_server commands are configurable
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/polonium/trunk@1746 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/polonium/server_runners/external_server_runner.rb
+++ lib/polonium/server_runners/external_server_runner.rb
@@ -1,25 +1,28 @@
 module Polonium
   module ServerRunners
     class ExternalServerRunner < ServerRunner
+      DEFAULT_START_SERVER_COMMAND = lambda do |configuration|
+        "cd #{configuration.rails_root}; script/server -e #{configuration.rails_env} -p #{configuration.internal_app_server_port} -c #{configuration.rails_root}"
+      end
+      DEFAULT_STOP_SERVER_COMMAND = lambda do |configuration|
+        "ps ax | grep 'script/server -e #{configuration.rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
+      end
+      
       class << self
         def start_server_command(&blk)
-#          if blk
-#            @start_server_command = blk
-#          else
-            @start_server_command ||= lambda do |configuration|
-              "cd #{configuration.rails_root}; script/server -e #{configuration.rails_env} -p #{configuration.internal_app_server_port} -c #{configuration.rails_root}"
-            end
-#          end
+          if blk
+            @start_server_command = blk
+          else
+            @start_server_command ||= DEFAULT_START_SERVER_COMMAND
+          end
         end
 
         def stop_server_command(&blk)
-#          if blk
-#            @stop_server_command = blk
-#          else
-            @stop_server_command ||= lambda do |configuration|
-              "ps ax | grep 'script/server -e #{configuration.rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
-            end
-#          end
+          if blk
+            @stop_server_command = blk
+          else
+            @stop_server_command ||= DEFAULT_STOP_SERVER_COMMAND
+          end
         end
       end
 
--- spec/polonium/server_runners/external_server_runner_spec.rb
+++ spec/polonium/server_runners/external_server_runner_spec.rb
@@ -3,29 +3,70 @@ require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 module Polonium
   module ServerRunners
     describe ExternalServerRunner do
-      attr_reader :configuration, :rails_env, :rails_root, :runner, :start_server_cmd, :stop_server_cmd
+      attr_reader :configuration, :rails_env, :rails_root, :runner, :start_server_command, :stop_server_command, :original_start_server_command, :original_stop_server_command
       before do
         @configuration = Configuration.new
         @rails_env = configuration.rails_env = 'test'
         @rails_root = configuration.rails_root = File.dirname(__FILE__)
-        @start_server_cmd = "cd #{rails_root}; script/server -e #{rails_env} -p #{configuration.internal_app_server_port} -c #{rails_root}"
-        @stop_server_cmd = "ps ax | grep 'script/server -e #{rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
+
+        @start_server_command = "cd #{rails_root}; script/server -e #{rails_env} -p #{configuration.internal_app_server_port} -c #{rails_root}"
+        @stop_server_command = "ps ax | grep 'script/server -e #{rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
 
         @runner = ExternalServerRunner.new(configuration)
       end
 
+      after do
+        ExternalServerRunner.start_server_command(&ExternalServerRunner::DEFAULT_START_SERVER_COMMAND)
+        ExternalServerRunner.stop_server_command(&ExternalServerRunner::DEFAULT_STOP_SERVER_COMMAND)
+      end
+
       describe "#start" do
         it "stops the server, then starts an external rails server" do
-          mock(runner).system(stop_server_cmd).ordered
-          mock(runner).system(start_server_cmd).ordered
+          mock(runner).system(stop_server_command).ordered
+          mock(runner).system(start_server_command).ordered
           runner.start
+        end
+
+        context "with a custom start_server_command" do
+          it "stops the server, then starts an external rails server with the custom command" do
+            ExternalServerRunner.start_server_command do
+              "custom start server command"
+            end
+
+            mock(runner).system(stop_server_command).ordered
+            mock(runner).system("custom start server command").ordered
+            runner.start
+          end
+        end
+
+        context "with a custom stop_server_command" do
+          it "stops the server with the custom command, then starts an external rails server" do
+            ExternalServerRunner.stop_server_command do
+              "custom stop server command"
+            end
+
+            mock(runner).system("custom stop server command").ordered
+            mock(runner).system(start_server_command).ordered
+            runner.start
+          end
         end
       end
 
       describe "#stop" do
         it "stops the server" do
-          mock(runner).system(stop_server_cmd).ordered
+          mock(runner).system(stop_server_command)
           runner.stop
+        end
+
+        context "with a custom stop_server_command" do
+          it "stops the server with the custom command" do
+            ExternalServerRunner.stop_server_command do
+              "custom stop server command"
+            end
+
+            mock(runner).system("custom stop server command")
+            runner.stop
+          end
         end
       end
     end
