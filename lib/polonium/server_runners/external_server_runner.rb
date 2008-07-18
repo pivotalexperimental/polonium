a437d30718ed6e0428ddfd696a09d96f94456f75
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-07-18 23:26:17 +0000

    Preparing to accept customizable start and stop server command.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/polonium/trunk@1745 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/polonium/server_runners/external_server_runner.rb
+++ lib/polonium/server_runners/external_server_runner.rb
@@ -1,10 +1,32 @@
 module Polonium
   module ServerRunners
     class ExternalServerRunner < ServerRunner
+      class << self
+        def start_server_command(&blk)
+#          if blk
+#            @start_server_command = blk
+#          else
+            @start_server_command ||= lambda do |configuration|
+              "cd #{configuration.rails_root}; script/server -e #{configuration.rails_env} -p #{configuration.internal_app_server_port} -c #{configuration.rails_root}"
+            end
+#          end
+        end
+
+        def stop_server_command(&blk)
+#          if blk
+#            @stop_server_command = blk
+#          else
+            @stop_server_command ||= lambda do |configuration|
+              "ps ax | grep 'script/server -e #{configuration.rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
+            end
+#          end
+        end
+      end
+
       protected
       def start_server
         stop_server
-        system("cd #{configuration.rails_root}; script/server -e #{configuration.rails_env} -p #{configuration.internal_app_server_port} -c #{configuration.rails_root}")
+        system(self.class.start_server_command.call(configuration))
       rescue Exception => e
         puts e.message
         puts e.backtrace
@@ -12,8 +34,7 @@ module Polonium
       end
 
       def stop_server
-        cmd = "ps ax | grep 'script/server -e #{configuration.rails_env}' | sed /grep/d | awk '{print $1}' | xargs kill -9 2>/dev/null"
-        system(cmd)
+        system(self.class.stop_server_command.call(configuration))
       end
     end
   end
