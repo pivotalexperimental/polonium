add7308b623516733ed655372d8c4bd0c7f92543
Brian Takita <grockit@Macpro03-desktop.(none)>
2008-08-04 17:43:50 -0700

    Created Polonium::Adapters::Rspec. Rspec adapeter handles latest version of rspec.

--- lib/polonium/adapters/rspec.rb
+++ lib/polonium/adapters/rspec.rb
@@ -1,18 +1,29 @@
-class Spec::Runner::Options
-  def stop_selenium(success)
-    Polonium::Configuration.instance.app_server_runner.stop
-    Polonium::Configuration.instance.stop_driver_if_necessary(success)
-    success
-  end
+module Polonium::Adapters
+  class Rspec
+    class << self
+      def stop_selenium(success)
+        configuration.app_server_runner.stop
+        configuration.stop_driver_if_necessary(success)
+        success
+      end
 
+      protected
+      def configuration
+        Polonium::Configuration.instance
+      end
+    end
+  end
+end
+
+class Spec::Runner::Options
   if instance_methods.include?('after_suite_parts')
     Spec::Example::ExampleGroup.after(:suite) do |success|
-      rspec_options.stop_selenium success
+      Polonium::Adapters::Rspec.stop_selenium success
     end
   else
     def run_examples_with_selenium_runner(*args)
       success = run_examples_without_selenium_runner(*args)
-      stop_selenium success
+      Polonium::Adapters::Rspec.stop_selenium success
       success
     end
     alias_method_chain :run_examples, :selenium_runner
